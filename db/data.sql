8b6db4921fc34d30512e269921791bafcb0fd272
Miguel <migueldelalibera9@gmail.com>
2024-12-23 13:39:10 -0300

    Adiciona data.sql

---
+++ db/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO `cadastro_estudante`.`estudante` (`nome`, `idade`, `serie`) VALUES
+('João Silva', 16, 1),
+('Maria Oliveira', 17, 3),
+('Carlos Pereira', 15, 1),
+('Ana Santos', 16, 2),
+('Lucas Costa', 18, 3);
+
+
+INSERT INTO `cadastro_estudante`.`notas` (`id_estudante`, `portugues`, `matematica`, `historia`, `geografia`, `fisica`, `quimica`, `biologia`, `ingles`) VALUES
+(1, 6.0, 5.0, 7.0, 6.5, 4.5, 6.0, 5.8, 5.7),
+(2, 10.0, 8.5, 9.4, 8.5, 8.7, 7.8, 8.9, 9.5),
+(3, 6.5, 9.0, 8.8, 7.5, 8.0, 6.2, 9.0, 9.2),
+(4, 8.2, 5.5, 7.0, 7.8, 6.0, 5.5, 6.5, 7.6),
+(5, 7.5, 7.5, 7.4, 8.0, 6.2, 6.8, 7.6, 8.5);
