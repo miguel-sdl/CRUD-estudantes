7b7ff9ca0ddf757a780d22b191c3b53e7beeb5e7
Miguel <migueldelalibera9@gmail.com>
2024-12-23 11:10:52 -0300

    fix Altera dependencia junit

---
+++ src/test/java/cadastroestudante/application/AppTest.java
@@ -0,0 +1,12 @@
+package cadastroestudante.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    @DisplayName("mainTest")
+    void main() {
+    }
+}
