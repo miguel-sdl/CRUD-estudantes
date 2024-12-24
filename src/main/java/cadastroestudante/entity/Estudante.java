88a67c7c0a1ed3e4119ba67cf45bb9bc0e7edc37
Miguel <migueldelalibera9@gmail.com>
2024-12-24 09:52:36 -0300

    Adiciona atributos as classes Estudante e Notas

--- src/main/java/cadastroestudante/entity/Estudante.java
+++ src/main/java/cadastroestudante/entity/Estudante.java
@@ -1,4 +1,14 @@
 package cadastroestudante.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Estudante {
+    int id;
+    String nome;
+    int idade;
+    int serie;
+    Notas notas;
 }
--- src/main/java/cadastroestudante/entity/Notas.java
+++ src/main/java/cadastroestudante/entity/Notas.java
@@ -1,4 +1,18 @@
 package cadastroestudante.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Notas {
+    int idEstudante;
+    double portugues;
+    double matematica;
+    double historia;
+    double geografia;
+    double fisica;
+    double quimica;
+    double biologia;
+    double ingles;
 }
