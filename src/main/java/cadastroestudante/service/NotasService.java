bdba52ad0b8873adb4c8318ab6d07595e6407744
Miguel <migueldelalibera9@gmail.com>
2024-12-28 12:04:12 -0300

    Adiciona funcionalidade save a NotasService

--- src/main/java/cadastroestudante/service/NotasService.java
+++ src/main/java/cadastroestudante/service/NotasService.java
@@ -34,7 +34,7 @@ public class NotasService {
     }
 
     private static void showFind() {
-        System.out.println("Digite o id do estudante para encontrar susa nota");
+        System.out.println("Digite o id do estudante para encontrar sua nota");
         int id = Integer.parseInt(SCANNER.nextLine());
 
         Notas notas = findByStudentId(id);
@@ -95,7 +95,55 @@ public class NotasService {
     }
 
     private static void showSave() {
+        System.out.println("Digite o id do estudante que tera suas notas salvas");
+        int id = Integer.parseInt(SCANNER.nextLine());
+        idValidate(id);
 
+        if (Objects.nonNull(findByStudentId(id))) {
+            System.out.println("As notas do estudante com id " + id + " ja existem");
+            return;
+        }
+
+        if (Objects.isNull(EstudanteService.findById(id))) {
+            System.out.println("Nao existe estudante com id " + id);
+            return;
+        }
+
+        System.out.println("Digite a nota de portugues");
+        double portugues = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de matematica");
+        double matematica = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de historia");
+        double historia = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de geografia");
+        double geografia = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de fisica");
+        double fisica = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de quimica");
+        double quimica = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de biologia");
+        double biologia = Double.parseDouble(SCANNER.nextLine());
+        System.out.println("Digite a nota de ingles");
+        double ingles = Double.parseDouble(SCANNER.nextLine());
+
+
+        Notas notas = Notas.builder()
+                .idEstudante(id)
+                .portugues(portugues)
+                .matematica(matematica)
+                .historia(historia)
+                .geografia(geografia)
+                .fisica(fisica)
+                .quimica(quimica)
+                .biologia(biologia)
+                .ingles(ingles)
+                .build();
+
+        int rowsAffected = save(notas);
+
+        if (rowsAffected == 1) {
+            System.out.println("Notas salva com sucesso");
+        }
     }
 
     private static void showDelete() {
@@ -112,6 +160,11 @@ public class NotasService {
         idValidate(notas.getIdEstudante());
         if (!notas.isValid()) throw new RuntimeException("Nota invalida");
         return NotasRepository.update(notas);
+    }
+
+    public static int save(Notas notas) {
+        if (!notas.isValid()) throw new RuntimeException("Nota invalida");
+        return NotasRepository.save(notas);
     }
 
     private static void idValidate(int id) {
